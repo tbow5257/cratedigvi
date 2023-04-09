91f81e16efcc5083b41b0c18b92dc69d1b7ebf14
Thomas Bowen <133tjb@gmail.com>
2023-04-09 16:45:45 -0700

    add prisma, react-query, mui and related packages
    - add albums route
    - add AlbumsTable component
    - add prisma schema
    - add react-query and jsx
    - add services dir, hook file
    - delete hello

---
+++ services/useAlbums.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "react-query";
+import { whippo_album } from "@prisma/client";
+
+const API_URL = "/api/albums";
+
+const getAlbums = async () => {
+
+  if (!API_URL) {
+    throw new Error("could not find API BASE URL, check your config");
+  }
+  const res = await fetch(API_URL, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch albums");
+  }
+
+  return await res.json()
+};
+
+export const useAlbums = () => useQuery<whippo_album[], Error>("albums", getAlbums);
